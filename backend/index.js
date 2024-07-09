c8a1837cdb212c1be76875010bdfc7570e298dfe
Hepheoluwah <mtoyin27@gmail.com>
2024-07-09 14:45:52 +0100

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -19,32 +18,11 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 
 // Connect to MongoDB
-=======
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
-const app = express()
-const Routes = require("./routes/route.js")
-
-const PORT = process.env.PORT || 5000
-
-dotenv.config();
-
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
-
->>>>>>> d60512a (Updated EduTrack)
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-<<<<<<< HEAD
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Connection to MongoDB failed", err));
 
@@ -55,13 +33,3 @@ app.use('/', Routes);
 app.listen(PORT, () => {
     console.log(`Server started at port no. ${PORT}`);
 });
-=======
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
-
-app.use('/', Routes);
-
-app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
->>>>>>> d60512a (Updated EduTrack)
